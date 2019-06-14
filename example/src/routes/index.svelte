862899b934f97291c8ddcd8021637f7c74ee30eb
FormBucket Support <support@formbucket.com>
2019-06-14 13:44:27 -0700

    Update example to use npm package.

--- example/src/routes/index.svelte
+++ example/src/routes/index.svelte
@@ -1,31 +1,9 @@
 <script>
-  import CodeMirror from "../../../src/CodeMirror.svelte";
+  import CodeMirror from "svelte-codemirror/src/CodeMirror.svelte";
 </script>
 
 <style>
-  h1,
-  figure,
-  p {
-    text-align: center;
-    margin: 0 auto;
-  }
 
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
-  }
 </style>
 
 <svelte:head>
