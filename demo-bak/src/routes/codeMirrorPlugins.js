04f57ab7aab6f15659c7ad19d872bc87ec81c40f
FormBucket Support <support@formbucket.com>
2019-06-18 19:11:46 -0700

    remove bak

--- demo-bak/src/routes/codeMirrorPlugins.js
+++
@@ -1,8 +0,0 @@
-import "codemirror/mode/javascript/javascript.js";
-// import 'codemirror/mode/shell/shell.js';
-import "codemirror/mode/handlebars/handlebars.js";
-import "codemirror/mode/htmlmixed/htmlmixed.js";
-import "codemirror/mode/xml/xml.js";
-import "codemirror/mode/css/css.js";
-import "codemirror/addon/edit/closebrackets.js";
-import "codemirror/addon/edit/closetag.js";
