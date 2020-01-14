5d9560a83af5e2b9e407f83043225b60d00e146b
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2020-01-14 17:19:59 +0000

    added export functions getSelection and getBlock

--- src/CodeMirror.svelte
+++ src/CodeMirror.svelte
@@ -53,47 +53,59 @@
     updating_externally = false;
   }
 
+
+  export function getSelection() {
+    if (editor) {
+      let expression = editor.getSelection();
+      if (expression == "") {
+        let cursorInfo = editor.getCursor();
+        expression = editor.getDoc().getLine(cursorInfo.line);
+      } 
+  }
+
   /*
    * Find code between dividers,
    * const divider = "__________";
   */  
   export function getBlock() {
     
-    let cursorInfo = editor.getCursor();
-    //find post divider
-    let line = cursorInfo.line;
-    let linePost = editor.lastLine();
+    if (editor) {
+      let cursorInfo = editor.getCursor();
+      //find post divider
+      let line = cursorInfo.line;
+      let linePost = editor.lastLine();
 
-    while (line < linePost) {
-      if (/___+/.test(editor.getLine(line))) {  // Test RegEx at least 3 underscores
-        linePost = line - 1;
-        break;
+      while (line < linePost) {
+        if (/___+/.test(editor.getLine(line))) {  // Test RegEx at least 3 underscores
+          linePost = line - 1;
+          break;
+        }
+        line++;
       }
-      line++;
-    }
 
-    line = cursorInfo.line;
-    let linePre = -1;
-    while (line >= 0) {
-      // console.log(editor2.getLine(line));
-      if (/___+/.test(editor.getLine(line))) {
-        linePre = line;
-        break;
+      line = cursorInfo.line;
+      let linePre = -1;
+      while (line >= 0) {
+        // console.log(editor2.getLine(line));
+        if (/___+/.test(editor.getLine(line))) {
+          linePre = line;
+          break;
+        }
+        line--;
       }
-      line--;
-    }
-    if (linePre > -1) {
-      linePre++;
-    }
-    let code = editor.getRange({
-      line: linePre,
-      ch: 0
-    }, {
-      line: linePost + 1,
-      ch: 0
-    });
+      if (linePre > -1) {
+        linePre++;
+      }
+      let code = editor.getRange({
+        line: linePre,
+        ch: 0
+      }, {
+        line: linePost + 1,
+        ch: 0
+      });
 
-    return code;
+      return code;
+    }
   }
 
 
