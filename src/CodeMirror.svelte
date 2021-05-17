8bca3ef21cc36a2d5be1a15d8cf47d79d119940d
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2021-05-18 00:38:03 +0100

    removed mousemouve handler

--- src/CodeMirror.svelte
+++ src/CodeMirror.svelte
@@ -428,13 +428,6 @@
       }
     });
 
-    editor.on("mousemove", (cm, ev) => {
-      console.log('cm mouse move')
-      ev.codemirrorIgnore = true;
-      ev.preventDefault();
-    });
-
-
     if (first) await sleep(50);
     editor.refresh();
 
